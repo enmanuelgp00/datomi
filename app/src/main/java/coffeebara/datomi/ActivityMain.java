faa1a04de3e0401644fd517941027061b096eef5
unknown <enmanuelgutierrezpoey@gmail.com>
2025-05-13 03:15:01 -0400

    Design: color green darck tones, top right total and current data

--- app/src/main/java/coffeebara/datomi/ActivityMain.java
+++ app/src/main/java/coffeebara/datomi/ActivityMain.java
@@ -38,111 +38,139 @@ import java.util.Set;
 
 
 public class ActivityMain extends Activity {
-   
-   MobileDataManager mobileDataManager;
-   TextView textViewOutput,
-	textViewDeadline,
-	textViewDays,
-	textViewBar;
-   Button buttonCheck;
-   Handler handler = new Handler(Looper.getMainLooper());
-   
-   public void onCreate(Bundle savedState) {
-      super.onCreate(savedState);      
-      setContentView(R.layout.activity_main);      
-      handlePermissions();
+	
+	MobileDataManager mobileDataManager;
+	TextView textViewOutput,
+		textViewDeadline,
+		textViewDays,
+		textViewBar,
+		textViewData;
+	Button buttonCheck;
+	Handler handler = new Handler(Looper.getMainLooper());
+	
+	public void onCreate(Bundle savedState) {
+		super.onCreate(savedState);		
+		setContentView(R.layout.activity_main);	
 
-      textViewOutput = findViewById(R.id.tv_log);
-      textViewDays = findViewById(R.id.tv_days);
-      textViewDeadline = findViewById(R.id.tv_deadline);
-      textViewBar = findViewById(R.id.tv_bar);
-      buttonCheck = findViewById(R.id.btn_check);
-      
 
-      mobileDataManager = new MobileDataManager(this);
-      buttonCheck.setOnClickListener(checkData());
+		handlePermissions();
+		textViewOutput = findViewById(R.id.tv_log);
+		textViewDays = findViewById(R.id.tv_days);
+		textViewData = findViewById(R.id.tv_data);
+		textViewDeadline = findViewById(R.id.tv_deadline);
+		textViewBar = findViewById(R.id.tv_bar);
+		buttonCheck = findViewById(R.id.btn_check);
 
-      textViewOutput.setOnClickListener( new View.OnClickListener(){
-         @Override
-         public void onClick(View view) {
+		mobileDataManager = new MobileDataManager(this);
+		buttonCheck.setOnClickListener(checkData());
 
-            Context context = getApplicationContext();
-            NotificationManager notificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
-            CharSequence name = "MyChannel";
-            String description = "Channel for My App";
-            int importance = NotificationManager.IMPORTANCE_DEFAULT;
-            NotificationChannel channel = new NotificationChannel("my_channel_id", name, importance);
-            channel.setDescription(description);
-            notificationManager.createNotificationChannel(channel);
+		textViewOutput.setOnClickListener( new View.OnClickListener(){
+			@Override
+			public void onClick(View view) {
 
-            Notification.Builder builder = new Notification.Builder(context, "my_channel_id");
-            builder.setSmallIcon(R.drawable.small_icon)
-               .setContentTitle("My Notification Title")
-               .setContentText("This is a notification message");
-            notificationManager.notify(1, builder.build());
-         }
-      });
-      
-      
-   } // end of onCreate();
-   
-   @Override
-   public void onResume() {
-      super.onResume();
-      mobileDataManager = new MobileDataManager(this);
-      display();
-   }
-   
-   @Override
-   public boolean onCreateOptionsMenu(Menu menu) {
-      getMenuInflater().inflate(R.menu.menu_main, menu);
-      return true;
-   }
-   @Override
-   public boolean onOptionsItemSelected(MenuItem menuItem) {
-      switch(menuItem.getItemId()) {
-         case R.id.menu_options:
-            startActivity(new Intent(ActivityMain.this, ActivityOptions.class));
-            break;
-         case R.id.menu_history:
-            startActivity(new Intent(getApplicationContext(), ActivityHistory.class));
-            break;
-         case R.id.menu_clear_today_data:
-            mobileDataManager.clearTodayData();
-            display();
-            Toast.makeText(getApplicationContext(), "Today recorded data cleaned",Toast.LENGTH_SHORT).show();
-            break;
-      }
-      return super.onOptionsItemSelected(menuItem);
-   }
-   public View.OnClickListener checkData() {
-      return new View.OnClickListener() {
-         @Override
-         public void onClick(View view) {     
-            // mobileDataManager = new MobileDataManager(getApplicationContext());       
-            buttonCheck.setEnabled(false);
-            buttonCheck.setText("Loading");
-            mobileDataManager.checkMobileData( new MobileDataManager.OnReceiveMobileData() {
-               @Override
-               public void onReceive(MobileData mobileData, String source) {
-                  Toast.makeText(ActivityMain.this, source, Toast.LENGTH_LONG).show();
-                  display();
-                  buttonCheck.setEnabled(true);
-                  buttonCheck.setText("Check");
-               }
-            });
-         }
-      };
-   } // end of checkData()
+				Context context = getApplicationContext();
+				NotificationManager notificationManager = (NotificationManager) getSystemService( Context.NOTIFICATION_SERVICE );
+				CharSequence name = "MyChannel";
+				String description = "Channel for My App";
+				int importance = NotificationManager.IMPORTANCE_DEFAULT;
+				NotificationChannel channel = new NotificationChannel("my_channel_id", name, importance);
+				channel.setDescription(description);
+				notificationManager.createNotificationChannel(channel);
 
-   public void handlePermissions() {
-      if (checkSelfPermission(Manifest.permission.CALL_PHONE) != PackageManager.PERMISSION_GRANTED) {
-         requestPermissions(new String[] {Manifest.permission.CALL_PHONE}, 1);
-      };
-   }
+				Notification.Builder builder = new Notification.Builder(context, "my_channel_id");
+				builder.setSmallIcon(R.drawable.small_icon)
+					.setContentTitle("My Notification Title")
+					.setContentText("This is a notification message");
+				notificationManager.notify(1, builder.build());
+			}
+		});
+
+	}
+	
+	@Override
+	public void onResume() {
+		super.onResume();
+		//mobileDataManager = new MobileDataManager(this);
+		//displayRefresh();
+		display();
+	}
+	
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		getMenuInflater().inflate(R.menu.menu_main, menu);
+		return true;
+	}
+	
+	@Override
+	public boolean onOptionsItemSelected(MenuItem menuItem) {
+		switch(menuItem.getItemId()) {
+			case R.id.menu_options:
+				startActivity(new Intent(ActivityMain.this, ActivityOptions.class));
+				break;
+			case R.id.menu_history:
+				startActivity(new Intent(getApplicationContext(), ActivityHistory.class));
+				break;
+			case R.id.menu_clear_today_data:
+				mobileDataManager.clearTodayData();
+				display();
+				Toast.makeText(getApplicationContext(), "Today recorded data cleaned",Toast.LENGTH_SHORT).show();
+				break;
+		}
+		return super.onOptionsItemSelected(menuItem);
+	}
+	public View.OnClickListener checkData() {
+		return new View.OnClickListener() {
+			@Override
+			public void onClick(View view) {
+				String loadingMessage = "...";
+				// mobileDataManager = new MobileDataManager(getApplicationContext());
+				CharSequence buttonCheckText = buttonCheck.getText();
+				buttonCheck.setEnabled(false);
+				buttonCheck.setText( loadingMessage );
+				mobileDataManager.checkMobileData( new MobileDataManager.OnReceiveMobileData() {
+					@Override
+					public void onReceive(MobileData mobileData, String source) {
+						Toast.makeText(ActivityMain.this, source, Toast.LENGTH_LONG).show();
+						display();
+						buttonCheck.setEnabled(true);
+						buttonCheck.setText(buttonCheckText);
+					}
+					@Override
+					public void onReceiveFailed( int failCode ) {
+						Toast.makeText(ActivityMain.this, "USSD receive failed: code : " + failCode , Toast.LENGTH_LONG).show();
+						buttonCheck.setEnabled(true);
+						buttonCheck.setText(buttonCheckText);
+					}
+				});
+			}
+		};
+	}
+
+	public void handlePermissions() {
+		if (checkSelfPermission(Manifest.permission.CALL_PHONE) != PackageManager.PERMISSION_GRANTED) {
+			requestPermissions(new String[] {Manifest.permission.CALL_PHONE}, 1);
+		};
+	}
+	void displayRefresh() {
+	Thread loop	= new Thread ( new Runnable() {
+			@Override
+			public void run () {
+				display();
+				try {
+					Thread.sleep( 5000 );
+				} catch ( Exception e ) {
+
+				}
+				displayRefresh();
+			}
+		});
+		//loop.start();
+		//handler.post( loop ); //is need if you update UI elements from onCreate();
+		//runOnUiThread( loop );
+	}
 
 	void display () {
-		final int BAR_SIZE = 32 ;
+		final int BAR_SIZE = 30 ;
 		if (mobileDataManager.getLogOfToday().size() > 0) {
 			StringBuilder log = new StringBuilder();
 			SimpleDateFormat simpleDateFormatter = new SimpleDateFormat("dd MMMM");
@@ -155,18 +183,22 @@ public class ActivityMain extends Activity {
 				log.append(MobileData.parseStringFormat(format, dataFormatter).asString());
 			}
 
-			long suggestion = mobileDataManager.todaySuggestionTillDeadline();
-			long used = mobileDataManager.todayDataBytesUsed();
+			long suggestion = mobileDataManager.getTodaySuggestionTillDeadline();
+			long used = mobileDataManager.getTodayDataBytesUsed();
 			int barProgress = (int) ( (double) used / suggestion * BAR_SIZE );
 			int remainingDays = mobileDataManager.getDaysTillDeadline();
 
 			textViewDays.setText(String.format("%d", remainingDays));
+			textViewData.setText( String.format( "%s > %s",
+				dataFormatter.format( mobileDataManager.getTodayLargerMobileData().getDataBytes()),
+				dataFormatter.format( mobileDataManager.getCurrentMobileData().getDataBytes())
+			));
 			textViewDeadline.setText(String.format("  %30s\n%s", 
 				simpleDateFormatter.format(mobileDataManager.getDeadline().getTime()),
 				retroBar( 30, 30 - remainingDays )
-//				retroBar( BAR_SIZE, BAR_SIZE - (int)( (float) remainingDays / 30 * BAR_SIZE) )
-			)); 
-			textViewBar.setText(String.format("\n  %-" + ( BAR_SIZE / 2 ) + "s%" + ( BAR_SIZE / 2 ) + "s \n%s",
+			));
+
+			textViewBar.setText(String.format("  %-" + ( BAR_SIZE / 2 ) + "s%" + ( BAR_SIZE / 2 ) + "s \n%s",
 				dataFormatter.format( used ),
 				dataFormatter.format( suggestion ),
 				retroBar( BAR_SIZE , barProgress )
@@ -175,12 +207,14 @@ public class ActivityMain extends Activity {
 			textViewOutput.setText(log.toString());	
 		} else {
 			textViewOutput.setText("No data recorded yet");
-			textViewDays.setText("");
+			textViewDays.setText("?");
 			textViewDeadline.setText("");
 			textViewBar.setText("");
 		}
 	}
+
 	
+
 	String retroBar ( int length, int progress ) {
 		StringBuilder bar = new StringBuilder();
 		
@@ -190,7 +224,8 @@ public class ActivityMain extends Activity {
 		int freeSpace = length - progress ;
 		bar.append("[ ");
 
-		if ( progress > length ) {		
+		if ( progress > length ) {
+			progressChar = ':';
 			int spaceAside = ( length - errorMessage.length() ) / 2 ;
 			StringBuilder aside = new StringBuilder();
 			for (int i = 0; i < spaceAside; i++ ) {
--- app/src/main/java/coffeebara/datomi/mobiledata/MobileDataManager.java
+++ app/src/main/java/coffeebara/datomi/mobiledata/MobileDataManager.java
@@ -6,6 +6,7 @@ import android.os.Looper;
 import android.os.Handler;
 import android.content.SharedPreferences;
 import android.widget.Toast;
+import android.net.TrafficStats;
 
 import java.util.Set;
 import java.util.TreeSet;
@@ -16,71 +17,82 @@ import java.text.SimpleDateFormat;
 
 public class MobileDataManager {
 
-    Handler mainLooper = new Handler(Looper.getMainLooper());
-    Context context;
-    SharedPreferences book;
-    SharedPreferences.Editor pen;
-    TelephonyManager telephonyManager;
-    TreeSet<String> responseHistory;
-    Calendar deadline;
-    MobileData previousMobileData, todayFirstMobileData, currentMobileData;
-    TreeSet<String> logOfKeys, logOfToday;
-    String keyLogOfToday;
+	Handler mainLooper = new Handler(Looper.getMainLooper());
+	Context context;
+	SharedPreferences book;
+	SharedPreferences.Editor pen;
+	TelephonyManager telephonyManager;
+	TreeSet<String> responseHistory;
+	Calendar deadline;
+	MobileData previousMobileData, todayFirstMobileData, currentMobileData;
+	TreeSet<String> logOfKeys, logOfToday;
+	String keyLogOfToday;
+	final String DATA_BYTES_USED_KEY = "DATA_BYTES_USED";
+	final String TRAFFIC_REFERENCE_KEY = "TRAFFIC_REFERENCE";
+	final String TRAFFIC_TOTAL_KEY = "TRAFFIC_TOTAL";
 
-    public MobileDataManager(Context context) {
-        this.context = context;
-        telephonyManager = (TelephonyManager) context.getSystemService(Context.TELEPHONY_SERVICE);
-        book = context.getSharedPreferences("book", Context.MODE_PRIVATE);
-        pen = book.edit();
+	public MobileDataManager(Context context) {
+		this.context = context;
+		telephonyManager = (TelephonyManager) context.getSystemService(Context.TELEPHONY_SERVICE);
+		book = context.getSharedPreferences("book", Context.MODE_PRIVATE);
+		pen = book.edit();
 
-        logOfKeys = getLogOfKeys(); 
+		logOfKeys = getLogOfKeys(); 
 
-        logOfToday = getLogOfToday(); 
-        todayFirstMobileData = getTodayFirstMobileData();
+		logOfToday = getLogOfToday(); 
+		todayFirstMobileData = getTodayFirstMobileData();
 		currentMobileData = todayFirstMobileData;
-        deadline = getDeadline();
-        if (logOfToday.size() > 0) {
-            previousMobileData = MobileData.parseStringFormat(logOfToday.last(), currentDataFormat());
-        } else if (logOfKeys.size() > 0) {
-            String yesterdayLast = new TreeSet<String>(book.getStringSet(logOfKeys.last(), null)).last();
-            previousMobileData = MobileData.parseStringFormat(yesterdayLast, currentDataFormat());
-        } else {
-            previousMobileData = new MobileData("", 0L, currentDataFormat());
-        }
-    }
+		deadline = getDeadline();
+		if (logOfToday.size() > 0) {
+			previousMobileData = MobileData.parseStringFormat( logOfToday.last(), currentDataFormat() );
+		} else if (logOfKeys.size() > 0) {
+			String yesterdayLast = new TreeSet<String>( book.getStringSet(logOfKeys.last(), null) ).last();
+			previousMobileData = MobileData.parseStringFormat( yesterdayLast, currentDataFormat() );
+		} else {
+			previousMobileData = new MobileData("", 0L, currentDataFormat());
+		}
+	}
 
-    public interface OnReceiveMobileData {
-        public void onReceive(MobileData mobileData, String source);
-    }
+	public interface OnReceiveMobileData {
+		public void onReceive(MobileData mobileData, String source);
+		public void onReceiveFailed( int failCode );
+	}
 
-    public void checkMobileData(OnReceiveMobileData onReceiveMobileData) {
-        telephonyManager.sendUssdRequest("*222#", new TelephonyManager.UssdResponseCallback() {
-            @Override
-            public void onReceiveUssdResponse(TelephonyManager telephonyManager, String request, CharSequence res) {
-                String keyOfToday = getKeyOfToday();
+	public void checkMobileData( OnReceiveMobileData onReceiveMobileData ) {
+		telephonyManager.sendUssdRequest("*222#", new TelephonyManager.UssdResponseCallback() {
 
-                currentMobileData = new MobileData(res.toString(), System.currentTimeMillis(), currentDataFormat());
-                store(logOfToday, keyOfToday, currentMobileData.getStringFormat());
-                if (!logOfKeys.contains(keyOfToday)) {
-                    store(logOfKeys, "logOfKeys", getKeyOfToday());
-                    todayFirstMobileData = getTodayFirstMobileData();
-                }
-                verifyDeadline(currentMobileData);
-                onReceiveMobileData.onReceive(currentMobileData, res.toString());
-            }
-        }, mainLooper);
-    } // end of checkMobileData
+			@Override
+			public void onReceiveUssdResponse(TelephonyManager telephonyManager, String request, CharSequence res) {
+				String todayKey = getKeyOfToday();
 
-    private void store(TreeSet<String> collection, String id, String info) {
-        collection.add(info);
-        pen.putStringSet(id, collection);
-        pen.apply();
-    }
+				currentMobileData = new MobileData(res.toString(), System.currentTimeMillis(), currentDataFormat());
+				store(logOfToday, todayKey, currentMobileData.getStringFormat());
+				if ( !logOfKeys.contains( todayKey ) ) {
+					store(logOfKeys, "logOfKeys", getKeyOfToday());
+					todayFirstMobileData = getTodayFirstMobileData();
+				}
+				checkDeadline( currentMobileData );
+				onReceiveMobileData.onReceive(currentMobileData, res.toString());
+			}
 
-    public TreeSet<String> getLogOfToday() {
-        return new TreeSet<String>(book.getStringSet(getKeyOfToday(), new TreeSet<String>()));
-    }
-    public TreeSet<String> getLogGlobal() {
+			@Override
+			public void onReceiveUssdResponseFailed(TelephonyManager telephonyManager, String request, int failCode ) {
+				onReceiveMobileData.onReceiveFailed( failCode );
+			}
+		}, mainLooper);
+	}
+
+	private void store(TreeSet<String> collection, String id, String info) {
+		collection.add(info);
+		pen.putStringSet(id, collection);
+		pen.apply();
+	}
+
+	public TreeSet<String> getLogOfToday() {
+		return new TreeSet<String>(book.getStringSet(getKeyOfToday(), new TreeSet<String>()));
+	}
+
+	public TreeSet<String> getLogGlobal() {
 		TreeSet<String> logGlobal = new TreeSet<String>();
 		for (String key : getLogOfKeys()) {
 			for (String log : book.getStringSet(key, new TreeSet<String>())) {
@@ -88,90 +100,117 @@ public class MobileDataManager {
 			}
 		}
 		return logGlobal;
-    }
+	}
 
-    public Calendar getDeadline() {
-        Calendar deadline = Calendar.getInstance();
-        deadline.setTimeInMillis(book.getLong("deadline", 0));
-        return deadline;
-    }
+	public Calendar getDeadline() {
+		Calendar deadline = Calendar.getInstance();
+		deadline.setTimeInMillis(book.getLong("deadline", 0));
+		return deadline;
+	}
 
-    public void setDeadline(Calendar date) {
-        this.deadline = date;
-        pen.putLong("deadline", date.getTimeInMillis());
-        pen.apply();
-    }
+	public void setDeadline(Calendar date) {
+		this.deadline = date;
+		pen.putLong("deadline", date.getTimeInMillis());
+		pen.apply();
+	}
 
-    private void verifyDeadline(MobileData currentMobileData) {
-        if (currentMobileData.getSmsBonus() > previousMobileData.getSmsBonus()) {
-            Calendar newDeadline = Calendar.getInstance();
-            newDeadline.setTime(currentMobileData.getCalendarDate().getTime());
-            newDeadline.add(Calendar.DAY_OF_MONTH, 30);
-            setDeadline(newDeadline);
-            Toast.makeText(context, "New deadline created ", Toast.LENGTH_LONG).show();
-        }
-        previousMobileData = currentMobileData;
-    }
+	private void checkDeadline( MobileData currentMobileData ) {
+		if (currentMobileData.getSmsBonus() > previousMobileData.getSmsBonus()) {
+			Calendar newDeadline = Calendar.getInstance();
+			newDeadline.setTime(currentMobileData.getCalendarDate().getTime());
+			newDeadline.add(Calendar.DAY_OF_MONTH, 30);
+			setDeadline(newDeadline);
+			Toast.makeText(context, "New deadline established", Toast.LENGTH_LONG).show();
+		}
+		previousMobileData = currentMobileData;
+	}
 
-    public int getDaysTillDeadline() {
-        long pointedDayMillis = getDeadline().getTimeInMillis();
-        long currentDayMillis = previousMobileData.getCalendarDate().getTimeInMillis();
-        long diffDaysMillis = pointedDayMillis - currentDayMillis;
-        return (int) (diffDaysMillis / (1000 * 60 * 60 * 24));
-    }
+	public int getDaysTillDeadline() {
+		long pointedDayMillis = getDeadline().getTimeInMillis();
+		long currentDayMillis = previousMobileData.getCalendarDate().getTimeInMillis();
+		long diffDaysMillis = pointedDayMillis - currentDayMillis;
+		return (int) (diffDaysMillis / (1000 * 60 * 60 * 24));
+	}
 
-    public DataFormat currentDataFormat() {
-        return new DataFormat(book.getInt("data_format", DataFormat.DECIMAL));
-    }
+	public DataFormat currentDataFormat() {
+		return new DataFormat(book.getInt("data_format", DataFormat.DECIMAL));
+	}
 
-    public void setDataFormat(DataFormat dataFormat) {
-        pen.putInt("data_format", dataFormat.getFormatType());
-        pen.apply();
-    }
+	public void setDataFormat(DataFormat dataFormat) {
+		pen.putInt("data_format", dataFormat.getFormatType());
+		pen.apply();
+	}
 
-    private MobileData getTodayFirstMobileData() {
-        if (logOfToday.size() > 0) {
-            return MobileData.parseStringFormat(logOfToday.first(), currentDataFormat());
-        } else {
-            return null;
-        }
-    }
+	private MobileData getTodayFirstMobileData() {
+		if (logOfToday.size() > 0) {
+			return MobileData.parseStringFormat( logOfToday.first(), currentDataFormat() );
+		} else {
+			return null;
+		}
+	}
+	public MobileData getCurrentMobileData() {
+		return previousMobileData;
+	}
+	public String getKeyOfToday() {
+		Calendar currentDate = Calendar.getInstance();
+		SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy MM dd");
+		return simpleDateFormat.format(currentDate.getTime());
+	}
 
-    public String getKeyOfToday() {
-        Calendar currentDate = Calendar.getInstance();
-        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy MM dd");
-        return simpleDateFormat.format(currentDate.getTime());
-    }
+	public TreeSet<String> getLogOfKeys() {
+		return new TreeSet<String>(book.getStringSet("logOfKeys", new TreeSet<String>()));
+	}
 
-    public TreeSet<String> getLogOfKeys() {
-        return new TreeSet<String>(book.getStringSet("logOfKeys", new TreeSet<String>()));
-    }
+	public long getTodaySuggestionTillDeadline() {
+		return todayFirstMobileData.getDataBytes() / getDaysTillDeadline() ;
+	}
 
-    public long todaySuggestionTillDeadline() {
-        return todayFirstMobileData.getDataBytes() / getDaysTillDeadline() + 1;
-    }
-    public MobileData todayLargerMobileData() {
-		if (currentMobileData.getDataBytes() > todayFirstMobileData.getDataBytes()) {
+	public MobileData getTodayLargerMobileData() {
+		if ( currentMobileData.getDataBytes() > todayFirstMobileData.getDataBytes() ) {
 			return currentMobileData;
 		} else {
 			return todayFirstMobileData;
 		}
-    }
+	}
 
-    public long todayDataBytesUsed() {
-        return todayFirstMobileData.getDataBytes() - previousMobileData.getDataBytes();
-    }
+	public long getTodayDataBytesUsed() {
 
-    public void clearAllData() {
-        pen.clear();
-        pen.commit();
-    }
-    public void clearTodayData() {
-        String todaysKey = getKeyOfToday();
-        logOfToday = new TreeSet<String>();
-        pen.putStringSet(todaysKey, logOfToday);
-        logOfKeys.remove(todaysKey);
-        pen.putStringSet("logOfKeys", logOfKeys);
-        pen.commit();
-    }
+		long savedDataBytesUsed = book.getLong( DATA_BYTES_USED_KEY, 0L );
+		long todayDataBytesUsed = todayFirstMobileData.getDataBytes() - previousMobileData.getDataBytes();
+		long currentMobileTraffic = TrafficStats.getMobileTxBytes() + TrafficStats.getMobileRxBytes();
+
+		if ( savedDataBytesUsed != todayDataBytesUsed ) {
+			pen.putLong( DATA_BYTES_USED_KEY, todayDataBytesUsed )
+				.putLong( TRAFFIC_REFERENCE_KEY, currentMobileTraffic )
+				.commit();
+			return todayDataBytesUsed;
+		}
+
+		if ( currentMobileTraffic != 0 ) {
+			pen.putLong( TRAFFIC_TOTAL_KEY, currentMobileTraffic );
+			pen.commit();
+		}
+		if ( book.getLong( TRAFFIC_REFERENCE_KEY, 0L ) == 0 ) {
+			pen.putLong( TRAFFIC_REFERENCE_KEY, currentMobileTraffic );
+			pen.commit();
+		}
+		return savedDataBytesUsed + book.getLong( TRAFFIC_TOTAL_KEY, 0L )  - book.getLong( TRAFFIC_REFERENCE_KEY, 0L );
+	}
+
+	public void clearAllData() {
+		pen.clear();
+		pen.commit();
+	}
+	public void clearTodayData() {
+		String todaysKey = getKeyOfToday();
+		logOfToday = new TreeSet<String>();
+		pen.putStringSet(todaysKey, logOfToday);
+		logOfKeys.remove(todaysKey);
+		pen.putStringSet("logOfKeys", logOfKeys)
+		.remove( DATA_BYTES_USED_KEY )
+		.remove( TRAFFIC_REFERENCE_KEY )
+		.remove( TRAFFIC_TOTAL_KEY );
+		pen.commit();
+	}
 }
+
